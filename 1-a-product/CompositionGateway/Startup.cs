ee16ab92199ed9942bc9580692646eaa855b43fb
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 13:08:58 +0200

    Composition gateway

---
+++ 1-a-product/CompositionGateway/Startup.cs
@@ -0,0 +1,33 @@
+﻿using ITOps.ViewModelComposition;
+using ITOps.ViewModelComposition.Gateway;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Divergent.CompositionGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.RunCompositionGateway(routes=> 
+            {
+                routes.MapComposableGet(template: "{controller}/{action}/{id:int?}");
+                routes.MapRoute("{*NotFound}", context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    return Task.CompletedTask;
+                });
+            });
+        }
+    }
+}
