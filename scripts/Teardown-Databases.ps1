aca1ac3eecd137a5271931fc59dcb8ee6836b43c
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 10:06:26 +0200

    Define instance

---
+++ scripts/Teardown-Databases.ps1
@@ -0,0 +1,9 @@
+﻿#Requires -RunAsAdministrator
+
+$instanceName = "microservices-done-right"
+
+$serverName = "(localdb)\" + $instanceName
+sqlcmd -S $serverName -i ".\Teardown-Databases.sql"
+
+sqllocaldb stop $instanceName
+sqllocaldb delete $instanceName
