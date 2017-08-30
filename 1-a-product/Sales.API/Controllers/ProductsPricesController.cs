cf6c3c98d26d343ac2014bbc5c28645ff8394f8a
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 11:21:38 +0200

    Sales REST API

---
+++ 1-a-product/Sales.API/Controllers/ProductsPricesController.cs
@@ -0,0 +1,41 @@
+﻿using Sales.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Sales.API.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsPricesController : ApiController
+    {
+        [HttpGet]
+        [Route("price/{id}")]
+        public dynamic Get(int id)
+        {
+            using (var db = new SalesContext())
+            {
+                var item = db.ProductsPrices
+                    .Where(o => o.ProductId == id)
+                    .SingleOrDefault();
+
+                return item;
+            }
+        }
+
+        [HttpGet]
+        [Route("prices/{ids}")]
+        public IEnumerable<dynamic> Get(string ids)
+        {
+            using (var db = new SalesContext())
+            {
+                var productIds = ids.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+                var items = db.ProductsPrices
+                    .Where(price => productIds.Any(id => id == price.ProductId))
+                    .ToArray();
+
+                return items;
+            }
+        }
+    }
+}
