09538dbec7b2d5ee112df8d120bdeea62b7d7a23
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 11:08:05 +0200

    Sales Data

--- scripts/Setup-Databases.sql
+++ scripts/Setup-Databases.sql
@@ -4,3 +4,7 @@ GO
 IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Marketing')
 CREATE DATABASE [1-Marketing]
 GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Sales')
+CREATE DATABASE [1-Sales]
+GO
--- scripts/Teardown-Databases.sql
+++ scripts/Teardown-Databases.sql
@@ -4,3 +4,7 @@ GO
 IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Marketing')
 DROP DATABASE [1-Marketing]
 GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Sales')
+DROP DATABASE [1-Sales]
+GO
