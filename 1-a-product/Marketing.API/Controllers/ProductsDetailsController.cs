4ab7be61c67627c5336eb4c1eb544326f8335134
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 10:59:25 +0200

    Marketing REST API

---
+++ 1-a-product/Marketing.API/Controllers/ProductsDetailsController.cs
@@ -0,0 +1,40 @@
+﻿using Marketing.Data.Context;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace Marketing.API.Controllers
+{
+    [RoutePrefix("api/orders")]
+    public class ProductsDetailsController : ApiController
+    {
+        [HttpGet]
+        public dynamic Get(int id)
+        {
+            using (var db = new MarketingContext())
+            {
+                var item = db.ProductsDetails
+                    .Where(o => o.ProductId == id)
+                    .SingleOrDefault();
+
+                return item;
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> Get(int pageIndex, int pageSize)
+        {
+            using (var db = new MarketingContext())
+            {
+                var items = db.ProductsDetails
+                    .OrderBy(p => p.Name)
+                    .Skip(pageSize * pageIndex)
+                    .Take(pageSize)
+                    .ToArray();
+
+                return items;
+            }
+        }
+    }
+}
---
+++ 1-a-product/Marketing.API/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace Marketing.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceHost>(s =>
+                {
+                    s.ConstructUsing(name => new ServiceHost());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalService();
+                x.StartAutomatically();
+
+                x.SetDescription("Microservices Done Right sample: Marketing API");
+                x.SetDisplayName("Marketing API");
+                x.SetServiceName("MarketingAPI");
+            });
+        }
+    }
+}
