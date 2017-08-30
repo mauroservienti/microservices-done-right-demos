4982da1bc42ca26915e14d3a0a8575091bb4fec5
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 13:16:49 +0200

    Scripts demo 2 support

--- scripts/Setup-Databases.sql
+++ scripts/Setup-Databases.sql
@@ -12,3 +12,15 @@ GO
 IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Warehouse')
 CREATE DATABASE [1-Warehouse]
 GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Marketing')
+CREATE DATABASE [2-Marketing]
+GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Sales')
+CREATE DATABASE [2-Sales]
+GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Warehouse')
+CREATE DATABASE [2-Warehouse]
+GO
--- scripts/Teardown-Databases.sql
+++ scripts/Teardown-Databases.sql
@@ -12,3 +12,16 @@ GO
 IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'1-Warehouse')
 DROP DATABASE [1-Warehouse]
 GO
+
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Marketing')
+DROP DATABASE [2-Marketing]
+GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Sales')
+DROP DATABASE [2-Sales]
+GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'2-Warehouse')
+DROP DATABASE [2-Warehouse]
+GO
