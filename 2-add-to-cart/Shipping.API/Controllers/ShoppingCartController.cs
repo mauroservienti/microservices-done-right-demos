185d8c1eb29af1772aab93e5da103828d31c287a
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-30 18:59:01 +0200

    Shipping Add To Cart support initial stage

---
+++ 2-add-to-cart/Shipping.API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace Shipping.API.Controllers
+{
+    public class ShoppingCartController : ApiController
+    {
+        [HttpPost]
+        [Route("api/shopping-cart")]
+        public IHttpActionResult AddToCart(dynamic data)
+        {
+            return StatusCode(HttpStatusCode.OK);
+        }
+    }
+}
