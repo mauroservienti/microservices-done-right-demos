da17e45811738327ec349a9511117c4bfc162bb2
Mauro Servienti <mauro.servienti@gmail.com>
2017-09-02 07:01:20 +0200

    Saga and Timeouts handling

---
+++ 5-sagas/Sales.Services/Program.cs
@@ -0,0 +1,42 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Sales.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpointName = "Sales.Services";
+            Console.Title = endpointName;
+            MainAsync(endpointName).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string endpointName)
+        {
+            var config = new EndpointConfiguration(endpointName);
+            config.UsePersistence<LearningPersistence>();
+            config.UseTransport<LearningTransport>();
+            config.UseSerialization<NewtonsoftSerializer>();
+            config.SendFailedMessagesTo("error");
+
+            var messageConventions = config.Conventions();
+            messageConventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Events"));
+            messageConventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages.Commands"));
+
+            var endpointInstance = await Endpoint.Start(config)
+                .ConfigureAwait(false);
+            try
+            {
+                Console.WriteLine($"\r\n{endpointName} bus created and configured; press any key to stop program\r\n");
+                Console.ReadKey();
+            }
+            finally
+            {
+                await endpointInstance.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ 5-sagas/Sales.Services/ShopingCartSaga.cs
@@ -0,0 +1,72 @@
+﻿using NServiceBus;
+using Sales.Messages.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Sales.Services
+{
+    class ShopingCartSaga : Saga<ShopingCartSaga.ShopingCartSagaData>,
+        IHandleMessages<ProductAddedToCart>,
+        IHandleTimeouts<CartGettingStaleTimeout>,
+        IHandleTimeouts<CartWipeTimeout>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<ShopingCartSagaData> mapper)
+        {
+            mapper.ConfigureMapping<ProductAddedToCart>(msg => msg.CartId).ToSaga(data => data.CartId);
+        }
+
+        public class ShopingCartSagaData : ContainSagaData
+        {
+            public int CartId { get; set; }
+            public DateTime LastTouched { get; set; }
+        }
+
+        public async Task Handle(ProductAddedToCart message, IMessageHandlerContext context)
+        {
+            Data.LastTouched = DateTime.Now;
+
+            await RequestTimeout(context, Data.LastTouched.AddDays(7), new CartGettingStaleTimeout()
+            {
+                LastTouched = Data.LastTouched
+            });
+
+            await RequestTimeout(context, Data.LastTouched.AddMonths(1), new CartWipeTimeout()
+            {
+                LastTouched= Data.LastTouched
+            });
+        }
+
+        public Task Timeout(CartGettingStaleTimeout state, IMessageHandlerContext context)
+        {
+            if (Data.LastTouched <= state.LastTouched)
+            {
+                //send e-mail
+                //publish event CartGotStaleDueToInactivity
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Timeout(CartWipeTimeout state, IMessageHandlerContext context)
+        {
+            if (Data.LastTouched <= state.LastTouched)
+            {
+                //publish event CartWipedDueToInactivity
+                //delete cart content
+                //MarkAsComplete();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    class CartGettingStaleTimeout
+    {
+        public DateTime LastTouched { get; set; }
+    }
+
+    class CartWipeTimeout
+    {
+        public DateTime LastTouched { get; set; }
+    }
+}
